7da293c390dc15071773c5adcf8d9d51d648f2cd
arjun-github <arjunsingh.gcet@gmail.com>
2017-04-22 12:46:35 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,20 @@
+
+public class output {
+	static int a=20;
+	static int b=10;
+
+	public static void main(String[] args) {
+		int c,d,e;
+		int b= a-- - --a;
+		System.out.println("b="+b);
+		c=a--;
+		System.out.println("c="+c);
+		d=a>>2;
+		System.out.println("d="+d);
+		e=a&b;
+		System.out.println("e="+e);
+		
+
+	}
+
+}
